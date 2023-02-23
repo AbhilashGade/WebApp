cd8ce60a74ebc11a3b4da4aa29b01cff2658e57c
AbhilashGade <gade.ab@northeastern.edu>
2023-02-23 12:32:32 -0500

    test build1

--- install-s.sh
+++ install-s.sh
@@ -5,7 +5,7 @@
     sleep 30
 
     sudo yum update -y
-    sudo yum upgrade -y
+
     sudo yum install -y gcc-c++ make
 
     curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
@@ -17,13 +17,13 @@
     sudo yum install mysql-community-server -y
     sudo systemctl start mysqld.service
     password=$(sudo cat /var/log/mysqld.log | grep "A temporary password" | awk '{print $NF}')
-
     sudo mysql -u root -p$password --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Abhilash@123';CREATE DATABASE userdb;USE userdb;"
 
     sudo yum install unzip -y
     cd ~/ && mkdir webapp
     sudo mv /home/ec2-user/webapp.zip /home/ec2-user/webapp/webapp.zip
     cd ~/webapp && unzip webapp.zip && npm i
+
     sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
     sudo systemctl enable webapp.service
     sudo systemctl start webapp.service
