7ae59e3c59e467335d77ddb454f73d78dc197378
AbhilashGade <gade.ab@northeastern.edu>
2023-03-02 12:21:35 -0500

    shellshr

--- install-s.sh
+++ install-s.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
-sudo yum update
-sudo yum upgrade
-sudo amazon-linux-extras install -y nginx1
-sudo amazon-linux-extras install -y epel
-# sudo yum remove libuv -y
-# sudo yum install libuv --disableplugin=priorities
-sudo yum install -y curl
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
-sudo yum install -y nodejs
 
+    # Install Node.js
 
-mkdir webapp
-mv webapp.zip webapp/
-cd webapp
-unzip webapp.zip
-rm webapp.zip
-# cd webapp
-npm install
-mkdir uploads
-cd ..
-sudo chmod 755 webapp
+    sleep 30
+
+    sudo yum update -y
+
+    sudo yum install -y gcc-c++ make
+
+    curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+    sudo yum install -y nodejs
+
+    # Install MySQL
+    sudo amazon-linux-extras install epel -y 
+    # sudo yum install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm -y
+    # sudo yum install mysql-community-server -y
+    # sudo systemctl start mysqld.service
+    # password=$(sudo cat /var/log/mysqld.log | grep "A temporary password" | awk '{print $NF}')
+    # sudo mysql -u root -p$password --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Abhilash@123';CREATE DATABASE userdb;USE userdb;"
+
+    sudo yum install unzip -y
+    cd ~/ && mkdir webapp
+    sudo mv /home/ec2-user/webapp.zip /home/ec2-user/webapp/webapp.zip
+    cd ~/webapp && unzip webapp.zip && npm i && mkdir uploads
+    
+
+    # sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
+    # sudo systemctl enable webapp.service
+    # sudo systemctl start webapp.service
